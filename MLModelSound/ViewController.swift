725ae38a822ff39b2ece877acbf08c4d75e2b3d2
techiro <techiromen0@gmail.com>
2020-08-02 14:17:51 +0900

    Initial Commit

---
+++ MLModelSound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLModelSound
+//
+//  Created by TanakaHirokazu on 2020/08/02.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
