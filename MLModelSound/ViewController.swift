7513a2882e61040bc96794ed46dc63198942e5e2
techiro <techiromen0@gmail.com>
2020-08-17 14:43:44 +0900

    全ての結果を表示

--- MLModelSound/ViewController.swift
+++ MLModelSound/ViewController.swift
@@ -11,7 +11,7 @@ import AVKit
 import SoundAnalysis
 
 protocol ClassifierDelegate {
-    func displayPredictionResult(identifier: String, confidence: Double)
+    func displayPredictionResult(data: String)
 }
 
 class ViewController: UIViewController {
@@ -74,6 +74,12 @@ class ViewController: UIViewController {
 
 
 extension ViewController: ClassifierDelegate {
+    func displayPredictionResult(data: String) {
+        DispatchQueue.main.async {
+            self.predictLabel.text = data
+        }
+    }
+    
     func displayPredictionResult(identifier: String, confidence: Double) {
         DispatchQueue.main.async {
             self.predictLabel.text = ("Recognition: \(identifier)\nConfidence \(confidence)")
@@ -88,15 +94,20 @@ class ResultsObserver: NSObject, SNResultsObserving {
     var delegate: ClassifierDelegate?
     
     func request(_ request: SNRequest, didProduce result: SNResult) {
-        guard let result = result as? SNClassificationResult,
-            let classification = result.classifications.first else { return }
+        guard let result = result as? SNClassificationResult else { return }
+        let classifications = result.classifications
+        var textResult = ""
         
-        
-        let confidence = classification.confidence*100
-        
-        if confidence > 60 {
-            delegate?.displayPredictionResult(identifier: classification.identifier, confidence: confidence)
+        for i in 0...classifications.count-1 {
+            let classification = classifications[i]
+            let identifier = classification.identifier
+            let confidence = classification.confidence*100
+            textResult += "\(identifier) \(confidence)\n"
+            
         }
+        
+        delegate?.displayPredictionResult(data: textResult)
+      
     }
     
     func request(_ request: SNRequest, didFailWithError error: Error) {
