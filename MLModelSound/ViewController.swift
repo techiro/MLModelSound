4c013d16c929c9e9c7d63ded8de9d966dbfbf8d2
techiro <techiromen0@gmail.com>
2020-08-20 14:42:21 +0900

    startAudioEngineの処理順番を最後に変更

--- MLModelSound/ViewController.swift
+++ MLModelSound/ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController {
         resultsObserver.delegate = self
         setUpAudioEngine()
         setUpAnalyzer()
-        startAudioEngine()
         startAnalyze()
+        startAudioEngine()
        
     }
     
